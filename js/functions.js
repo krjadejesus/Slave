5fc7b398f110f6b373be2dad1307e233f0ba42a0
King <krjadejesus@gmail.com>
2020-05-11 18:43:04 +0800

    added some JS examples

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+//functions
+var whatIsMyFavColor = function  () {
+
+}
+
+var doSomething = function  () {
+	console.log('Sample Text');
+}
---
+++ js/variables.js
@@ -0,0 +1,11 @@
+//comment here
+var favColor = 'red';
+var favColors - ['blue', 'red'] //this is an array
+var numOfFavColors = 3;
+var	hasGotFavColors = true;
+var kingObject = {
+	firstName: 'King',
+	myFavColors: ['black', 'white'],
+	yearsAlive: 31,
+	isMale: true
+};

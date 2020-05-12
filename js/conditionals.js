6f964d9e2a161083c04ad36018b708042c5a2c02
King <krjadejesus@gmail.com>
2020-05-12 19:04:41 +0800

    added if statements

---
+++ js/conditionals.js
@@ -0,0 +1,16 @@
+//conditional statements
+
+if (true) {
+	alert('male');
+} 
+
+var num1 = 2000;
+var num2 = 260;
+
+if (num1 == num2) {
+	alert('The same number!');
+} else if (num1 < num2) {
+	alert('Num1 is less than Num2');
+} else {
+	alert('Num1 is greater than Num2');
+}
--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,13 @@
 //functions
-var whatIsMyFavColor = function  () {
+var area = function (width, height) {
+	return width * height;
+}
 
+var fullName = function (firstName, lastName) {
+	return firstName + ' ' + lastName;
+}
+var whatIsMyFavColor = function  () {
+	return 'pink';
 }
 
 var doSomething = function  () {
